724f2e3cb2127f9d46eb9e7c5ad6848f8d83cdc7
Lancelot <skmburu110@gmail.com>
2023-07-24 01:52:51 -0700

    removed my putchar,defined buffer,included print_buff

--- main.h
+++ main.h
@@ -2,9 +2,15 @@
 #define MAIN_H
 
 
-int _putchar(char c);
-int _printf(const char *format, ...);
+#include <stdio.h>
+#include <stdlib.h>/*for malloc*/
+#include <stdarg.h>/*for variadic functions*/
 
+#define BUFF_SIZE 1024/*where write output is to be stored*/
+
+int _printf(const char *format, ...);
+void print_buff(char buffer[], int *buff_ind);
+int _putchar(char c);
 
 
 #endif /* MAIN_H */
